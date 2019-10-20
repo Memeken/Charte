64df39a4a589316fe2a396244832cb2d17ca0525
Memeken <coralie2319@hotmail.fr>
2019-10-20 20:26:11 +0200

    modif du 20/10

--- db.php
+++ db.php
@@ -1,23 +1,16 @@
 <?php
-
-	//$userCredentials =$_POPST;
-	$name = $_POST['name'];
-
 	try {
-		$connexion = new PDO("mysql:host=localhost;dbname=charte;charset=utf8","identifiant","motdepasse");
-		$connexion->query('INSERT INTO apprenants (name) VALUES ($name)');
-
-	//->fetchAll();
-
-	//echo "connect to db";
-	//var_dump($result);
-	//foreach ($result as $user){}
-	//echo $user ['pseudo'] . <"br">
-
-		
-	} catch (Exception $error){
-		echo $error->getMessage();
-		
+		$bdd= new PDO("mysql:host=localhost;dbname=CHARTE","damery","damery");
 	}
+		
+	catch(PDOException $error){
+	echo "erreur" . $error-> getMessages(); // message test pour voir si la bdd est connectee (tu le marques tel quel)
+	}
+
+	$nom = $_POST['nom']; // on crée la variable pour faire l'action: nom est égal à ce qu on va renseigner dans le bouton 'nom'
+
+	$prenom = $_POST['prenom'];// 'nom', 'prenom', 'age' ceux sont les 'name' de nos input ci dessous
+
+	$insert = $bdd->query("INSERT INTO SIGNATURE VALUES ('$nom' , '$prenom')");
 	
 ?>
--- index.php
+++ index.php
@@ -10,14 +10,19 @@ include "db.php";
     <meta name=viewport content="width=device-width, initial-scale=1">
     <title>Charte des apprenants</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Zhi+Mang+Xing&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap" rel="stylesheet"> 
 </head>
 
     <body>
-        <h1>Charte de l'apprenant.e</h1>
         
-            <h2>Fonctionnement global de la promo / Boon of the tiger</h2>
-            
-                <ul>
+        <h1>Charte de l'apprenant.e</h1> 
+        <div id="conteneur">  
+
+            <div class="block">
+            <h2 class="title">Fonctionnement global de la promo / Boon of the tiger</h2>
+                <ul class="img">
                     <li>le confort de tou.te.s en compte tu prendras</li>
                     <li>des locaux propres tu préserveras</li>
                     <li>collectivement le vendredi tu rangeras</li>
@@ -26,10 +31,11 @@ include "db.php";
                     <li>la feuille de présence tu signeras</li>
                     <li>dans le travail concentré tu seras</li>
                 </ul>
-        
-            <h2>Etat d'esprit / vision / Hidden war</h2>
-            
-                <ul>
+            </div>
+
+            <div class="block">
+            <h2 class="title">Etat d'esprit / vision / Hidden war</h2>
+                <ul class="img">
                     <li>à la réussite collective tu oeuvreras</li>
                     <li>plus haut que ton cul ne pèteras</li>
                     <li>quand la parole tu prendras l'attention tu auras</li>
@@ -40,10 +46,11 @@ include "db.php";
                     <li>ton esprit ouvert tu garderas</li>
                     <li>en autonomie tu feras mais l'esprit de groupe tu protègeras</li>
                 </ul>
-        
-            <h2>Engagements de l'apprenant.e / ligthningcrushers</h2>
-            
-                <ul>
+            </div>
+
+            <div class="block">
+            <h2 class="title">Engagements de l'apprenant.e / ligthningcrushers</h2>
+                <ul class="img">
                     <li>assidu tu seras</li>
                     <li>le temps de parole tu respecteras</li>
                     <li>tes formateurs tu respecteras</li>
@@ -54,11 +61,20 @@ include "db.php";
                     <li>ces règles tu appliqueras</li>
                     <li>le matériel tu respecteras</li>
                 </ul>
+            </div>
+
+        </div>
+            
 
             <form action="index.php" method="post" class="Nom">
                 <div class="form-example">
                     <label for="name">Nom: </label>
-                    <input type="text" name="name" id="name" required placeholder="Nom">
+                    <input type="text" name="nom" id="name" required placeholder="Nom">
+                </div>
+                <br>
+                <div class="form-example">
+                    <label for="prenom">Prénom: </label>
+                    <input type="text" name="prenom" id="prenom" required placeholder="Prénom">
                 </div>
                 <br>
                 <div class="form-example">
@@ -71,12 +87,13 @@ include "db.php";
                 <div>
                    
                     <?php
-                    foreach ($name as $element) {
-                        echo $element . "<br>";
+                    //foreach ($name as $element) {
+                        //echo $element . "<br>";
                         # code...
-                    }
+                    //}
+                    echo $nom ." ". $prenom;
                     ?>
-                </div>>
+                </div>
                   
                         
                
